0bf689ea4ccb6f9948ebab9c273d74de98f36c0e
Irina Tokareva <itokareva@yandex.ru>
2021-05-23 00:45:47 +0300

    add kubernetes-vault/

---
+++ kubernetes-vault/infra/outputs.tf
@@ -0,0 +1,19 @@
+output "name" {
+  # This may seem redundant with the `name` input, but it serves an important
+  # purpose. Terraform won't establish a dependency graph without this to interpolate on.
+  description = "The name of the cluster master. This output is used for interpolation with node pools, other modules."
+
+  value = google_container_cluster.ladoga.name
+}
+
+output "master_version" {
+  description = "The Kubernetes master version."
+  value       = google_container_cluster.ladoga.master_version
+}
+
+output "endpoint" {
+  description = "The IP address of the cluster master."
+  sensitive   = true
+  value       = google_container_cluster.ladoga.endpoint
+}
+
